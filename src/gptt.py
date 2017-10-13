dfbb6f74df33755288195f102591287496f69428
mauimuc <mauerber@uni-potsdam.de>
2017-10-13 09:03:13 +0200

    Just skeleton Files

---
+++ src/gptt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__    = "Stefan Mauerberger"
+__copyright__ = "Copyright (C) 2017 Stefan Mauerberger"
+__license__   = "GPLv3"
+
+import numpy as np
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
