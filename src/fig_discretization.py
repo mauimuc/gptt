0e7bf068557c5b469ac1f578aed86e4fdb38afbc
mauimuc <mauerber@uni-potsdam.de>
2017-11-13 21:20:30 +0100

    Figure fig_discretization.pgf got its own script

---
+++ src/fig_discretization.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__    = "Stefan Mauerberger"
+__copyright__ = "Copyright (C) 2017 Stefan Mauerberger"
+__license__   = "GPLv3"
+
+''' Save a plot of the discretization as PGF file '''
+
+from matplotlib import pyplot as plt
+from plotting import rcParams, prepare_map
+from example import stations, points
+
+plt.rcParams.update(rcParams)
+
+# Prepare map
+fig = plt.figure(figsize=(3,3))
+m = prepare_map()
+
+m.scatter(stations['lon'], stations['lat'], lw=0, color='g', latlon=True)
+# Plot discretization
+m.scatter(points['lon'], points['lat'], lw=0, marker='.', s=4, latlon=True, color='g', rasterized=True)
+
+plt.savefig('../fig_discretization.pgf')
+
