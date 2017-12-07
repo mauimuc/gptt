28680a5a9608c3bbe48971a72949ce03ca266c0b
mauimuc <mauerber@uni-potsdam.de>
2017-12-07 13:41:29 +0100

    Plots became a map-scale

--- src/plotting.py
+++ src/plotting.py
@@ -35,16 +35,21 @@ lllat = stations['lat'].min() - 0.5
 urlon = stations['lon'].max() + 1
 urlat = stations['lat'].max() + 0.5
 
+lat_0 = 67
+lon_0 = 17
 
 def prepare_map(ax=None, pls=[1,0,0,0]):
     ''' Prepare map and plot coast lines, parallels and meridians and returns the Basemap object.
         The argument pls is more a hack for having plots side by side. '''
     m = Basemap(llcrnrlon=lllon, llcrnrlat=lllat, urcrnrlon=urlon, urcrnrlat=urlat,\
                 resolution='i', projection='merc', \
-                lat_0=65, lon_0=17, lat_ts=50, ax=ax)
+                lat_0=lat_0, lon_0=lon_0, lat_ts=50, ax=ax)
     m.drawcoastlines(color='gray', linewidth=0.5)
     m.drawparallels((65,69), labels=pls,       linewidth=0.5, dashes=(2,2))
     m.drawmeridians((15,20), labels=[0,0,1,0], linewidth=0.5, dashes=(2,2))
+    scl = m.drawmapscale(urlon-1, urlat-0.25, lon_0, lat_0, 100, fontsize=6)
+    scl[0].set_linewidth(0.5)
+
     return m
 
 
